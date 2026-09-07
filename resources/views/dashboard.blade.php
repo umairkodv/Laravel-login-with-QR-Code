da4390b740e11a5cd6e57c1b5206577d4a602d99
Umair Zubair <134955307+umairkodv@users.noreply.github.com>
2026-09-07 22:02:47 +0500

    changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,8 +4,7 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
-    
+    dd
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
